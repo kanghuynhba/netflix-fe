e05381e5f6fb6eb4ac08fe9aedf3bcb034cb58d3
khang-huynh-ba <kanghuynhba@gmail.com>
2023-03-17 18:34:42 +0700

    add header & add billboard

--- src/layout/components/Header/Search/index.js
+++ src/layout/components/Header/Search/index.js
@@ -1,23 +1,58 @@
-import { faClose, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState, useRef } from 'react';
+import { faClose } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import { SearchIcon } from '~/components/Icons';
 import styles from './Search.module.scss';
 
 const cx = classNames.bind(styles);
+
 function Search() {
+    const [isOpenSearch, setIsOpenSearch] = useState(false);
+    const [searchValue, setSearchValue] = useState('');
+
+    const inputRef = useRef();
+
+    const handleOpenSearch = () => {
+        setIsOpenSearch(!isOpenSearch);
+    };
+    const handleClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+    };
+    const handleClosed = (e) => {
+        e.stopPropagation();
+    };
+    useEffect(() => {
+        const handleClick = () => {
+            if (isOpenSearch) {
+                setIsOpenSearch(!isOpenSearch);
+            }
+        };
+        window.addEventListener('click', handleClick);
+        return () => {
+            window.removeEventListener('click', handleClick);
+        };
+    }, [isOpenSearch]);
     return (
-        <div className={cx('search-box')}>
-            <button className={cx('search-btn')}>
-                <SearchIcon />
+        <div className={cx('search-box')} onClick={handleClosed}>
+            <button className={cx('search-btn')} onClick={handleOpenSearch}>
+                <SearchIcon className={cx('search-icon')} />
             </button>
-            <div className={cx('search-input')}>
-                <SearchIcon />
-                <input placeholder="Titles,people,genres" />
-                <span className={cx('close-icon')}>
+            <input
+                ref={inputRef}
+                value={searchValue}
+                onChange={(e) => {
+                    setSearchValue(e.target.value);
+                }}
+                className={cx('search-input', `${isOpenSearch ? 'toggle' : ''}`)}
+                placeholder="Titles, people, genres"
+            />
+            {!!searchValue && (
+                <span className={cx('clear')} onClick={handleClear}>
                     <FontAwesomeIcon icon={faClose} />
                 </span>
-            </div>
+            )}
         </div>
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Home.module.scss';
+import BillBoard from './BillBoard';
+
+const cx = classNames.bind(styles);
 function Home() {
-    return <h2>Home page</h2>;
+    return (
+        <div className={cx('main-view')}>
+            <BillBoard />
+        </div>
+    );
 }
 
 export default Home;
