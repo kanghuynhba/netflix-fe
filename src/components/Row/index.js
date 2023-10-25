08974c5da8fb702576e7618d1b9884e23bff0b44
Khang Huynh Ba <kanghuynhba@gmail.com>
2023-10-25 14:28:30 +0700

    Change name web tag

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -2,13 +2,16 @@
 import classNames from 'classnames/bind';
 import styles from './Row.module.scss';
 import SliderItem from './SliderItem';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import * as rowService from '~/services/rowService';
-import requests from '~/utils/routes';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
+const handleNext = () => {};
 function Row({ title, fetch }) {
     const [movies, setMovies] = useState([]);
+    const listRef = useRef();
     // Get API Row
     useEffect(() => {
         const fetchApi = async () => {
@@ -22,11 +25,14 @@ function Row({ title, fetch }) {
     return (
         <div className={cx('wrapper')}>
             <h2 className={cx('row-header')}>{title}</h2>
-            <div className={cx('slider-container')}>
+            <div className={cx('slider-container')} ref={listRef}>
                 {movies.map((movie) => (
-                    <SliderItem key={movie?.id} movie={movie} />
+                    <SliderItem key={movie?.id} movie={movie ? movie : ''} />
                 ))}
             </div>
+            <span className={cx('next')} onClick={handleNext}>
+                <FontAwesomeIcon icon={faAngleRight} />
+            </span>
         </div>
     );
 }
