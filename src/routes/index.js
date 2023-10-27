613c4019259f9b29c28dd2fd9f41aca00d3bf272
Khang Huynh Ba <kanghuynhba@gmail.com>
2023-10-27 14:57:06 +0700

    fix bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import config from '~/config';
 
 // Pages
+import Account from '~/pages/Account';
 import Home from '~/pages/Home';
 import Movies from '~/pages/Movies';
 import TVShows from '~/pages/TVShows';
 
 // Public routes
 const publicRoutes = [
+    { path: config.routes.home, component: Account },
     { path: config.routes.browse, component: Home },
     { path: config.routes.movies, component: Movies },
     { path: config.routes.tvshows, component: TVShows },
